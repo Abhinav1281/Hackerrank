3a1569a1fba5bb9b6f5c56135e39b5c7f534f39f
Abhinav1281 <abhisione2@gmail.com>
2020-07-12 19:44:23 +0530

    birdcounting commit 12th july

---
+++ birdcounting.java
@@ -0,0 +1,32 @@
+package Hackerrank;
+import java.util.*;
+import basics.*;
+public class birdcounting {
+
+	
+		static int migratoryBirds(List<Integer> arr)
+		{
+	        int valarr[]={0,0,0,0,0};
+	        for(int i=0;i<arr.size();i++)
+	        {
+	            valarr[(arr.get(i)-1)]++;
+	        }
+	        int min=1;
+	        for(int i=1;i<5;i++)
+	        {
+	            if(valarr[i]>valarr[(min-1)])
+	                min=i+1;
+	        }
+	        return min;
+
+
+	    }
+
+		public static void main(String[] args) 
+		{
+			List<Integer> ll = Arraylistini.input();
+			int val=migratoryBirds(ll);
+			System.out.println(val);
+
+}
+}
